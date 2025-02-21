6880683c2cf84fd0219b45fa83a4aff36ba26d96
Андрей <andrey.mcrtychyan@yandex.ru>
2025-02-21 13:19:28 +0300

    21.02 сделал сайт

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuBtn = document.querySelector('.menu__btn');
 
     menuBtn.addEventListener('click', function() {
+        menuBtn.classList.toggle('menu__btn_active');
         mobileMenu.classList.toggle('menu--open');
         document.documentElement.classList.toggle('active');
     });

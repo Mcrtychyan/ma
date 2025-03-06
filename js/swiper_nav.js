fba975c9c4e66eef8b3504c655c8def747e958b5
Андрей <andrey.mcrtychyan@yandex.ru>
2025-03-06 13:38:12 +0300

    не получилось сделать еще одно скроливание

---
+++ js/swiper_nav.js
@@ -0,0 +1,24 @@
+new Swiper('.swiper', {
+
+    loop: true,
+
+    loop: true,
+    spaceBetween: 20,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.cases__btn--next',
+      prevEl: '.cases__btn--prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  
+})
